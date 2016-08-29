71b4f8fc37c91d33947839aae924d5416251d15b
Matt Kraatz <matt.kraatz@gmail.com>
2016-08-28 19:32:19 -0500

    refactored with jquery instead of css

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -87,6 +87,28 @@ $(document).on('click', '.showAll', function() {
     $('.movieCard').css('display', 'inline-block');
 });
 
+// STAR HOVER FUNCTIONALITY
+
+$(document).on({
+  mouseenter: starHoverOn,
+  mouseleave: starHoverOff
+},'i');
+
+function starHoverOn(evt) {
+  let $hoverStar = $(evt.currentTarget);
+  $hoverStar.addClass('current-star hover-star');
+  $hoverStar.siblings().addClass('hover-star');
+  $('.current-star ~ i').removeClass('hover-star');
+  $('.current-star ~ i').addClass('black-star');
+}
+
+function starHoverOff(evt) {
+  let $hoverStar = $(evt.currentTarget);
+  $hoverStar.removeClass('current-star hover-star');
+  $hoverStar.siblings().removeClass('hover-star');
+  $hoverStar.siblings().removeClass('black-star');
+}
+
 // UPDATE SEEN MOVIES IN PROFILE
 $(document).on('click', '.watchedMovieProfile', updateWatchedMovie);
 $(document).on('click', '.userRating', updateRating);
--- src/js/template.js
+++ src/js/template.js
@@ -1,16 +1,16 @@
 "use strict";
 
 let searchMovies = require('./../templates/findMovies.hbs'),
-    showSavedMovies = require('./../templates/savedMovies.hbs');
-var Handlebars = require('hbsfy/runtime');
+    showSavedMovies = require('./../templates/savedMovies.hbs'),
+    Handlebars = require('hbsfy/runtime');
 
 Handlebars.registerPartial('searchResults', require('./../templates/partials/searchResults.hbs'));
 
 Handlebars.registerHelper('activateStars',function(rating, index, options){
   if (rating >= index) {
-    return 'active-star'
+    return 'active-star';
   }
-})
+});
 
 function showSearchResults (searchData){
   $('.display').html(searchMovies(searchData));
