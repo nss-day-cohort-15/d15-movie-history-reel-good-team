e8407a24345e2a4337a1392f5a7bb7d735f51a6b
Delaine Wendling <delaine.wendling@gmail.com>
2016-08-29 22:15:00 -0500

    Changed the functionality so that the watched, unwatched, and favorite movies are coming from database rather than filter

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -191,3 +191,61 @@ function updateRating (e){
   });
 }
 
+//Button functionality
+$(document).on('click', '.showUntracked', function(evt) {
+  $('.filter').removeClass('cyan lighten-4');
+  let $activeFilter = $(evt.currentTarget);
+  $activeFilter.siblings().removeClass('active-button');
+  $activeFilter.addClass('cyan lighten-4');
+  $('.secondBread').html('Untracked');
+  $('.movieDiv').show();
+  $('.movieCard[saved=true]').parent('.movieDiv').hide();
+});
+
+$(document).on('click', '.showUnwatched', function(evt) {
+  db.getSavedMovies()
+  .then(function(data){
+    template.showProfile(data);
+    checkNumberOfMovies();
+    $('.filter').removeClass('cyan lighten-4');
+    let $activeFilter = $(evt.currentTarget);
+    $activeFilter.siblings().removeClass('active-button');
+    $activeFilter.addClass('cyan lighten-4');
+    $('.secondBread').html('Unwatched');
+    $('.movieDiv').hide();
+    $('.movieCard[rating=0]').parent('.movieDiv').show();
+  });
+});
+
+$(document).on('click', '.showWatched', function(evt) {
+   db.getSavedMovies()
+  .then(function(data){
+    template.showProfile(data);
+    checkNumberOfMovies();
+    $('.filter').removeClass('cyan lighten-4');
+    let $activeFilter = $(evt.currentTarget);
+    $activeFilter.siblings().removeClass('active-button');
+    $activeFilter.addClass('cyan lighten-4');
+    $('.secondBread').html('Watched')
+    $('.movieDiv').hide();
+    $('.movieCard[saved=true]').parent('.movieDiv').show();
+    $('.movieCard[rating=0]').parent('.movieDiv').hide();
+  });
+});
+
+$(document).on('click', '.showFavorites', function(evt) {
+   db.getSavedMovies()
+  .then(function(data){
+    template.showProfile(data);
+    checkNumberOfMovies();
+    $('.filter').removeClass('cyan lighten-4');
+    let $activeFilter = $(evt.currentTarget);
+    $activeFilter.siblings().removeClass('active-button');
+    $activeFilter.addClass('cyan lighten-4');
+    $('.secondBread').html('Favorites')
+    $('.movieDiv').hide();
+    $('.movieCard[rating=10]').parent('.movieDiv').show();
+  });
+});
+
+
